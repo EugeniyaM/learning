73183c5235132b516d40853b724bbb87701ea07b
Jane <volegzhaninaem@list.ru>
2021-03-18 08:07:04 +0700

    Proxy commit 3

--- proxy.js
+++ proxy.js
@@ -99,9 +99,61 @@ const data = withHiddenProps({
 
 // Optimization
 
-const userData = [
+const IndexArray = new Proxy(Array, {
+  construct(target, [args]) {
+    const index = {}
+    args.forEach(item => (index[item.id] = item))
+
+    return new Proxy(new target(...args), {
+      get(arr, prop) {
+        switch (prop) {
+          case 'push':
+            return item => {
+              index[item.id] = item
+              arr[prop].call(arr, item)
+            }
+            case 'findById':
+              return id => index[id]
+          default:
+            return arr[prop]
+        }
+      }
+    })
+  }
+})
+
+const users = new IndexArray([
   {id: 1, name: 'Jane', job: 'Fullstack', age: 25},
   {id: 2, name: 'Elena', job: 'Student', age: 20},
   {id: 3, name: 'Victor', job: 'Backend', age: 23},
   {id: 4, name: 'Vasilisa', job: 'Teacher', age: 24}
-]
+])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
